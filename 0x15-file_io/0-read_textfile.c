f6c04e7b4ff11aeb6cf0d606bd9939307a90a3b2
alphonsi <apphonsio@yahoo.com>
2023-07-10 10:02:30 -0700

    created 0x15-file_io dir

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, x, y;
+	char *buf;
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+	x = read(fd, buf, letters);
+	if (x < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[x] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, x);
+	if (y < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (y);
+}
